bdfb6f6ebb432bc34af30821552a51cba0fbf79d
stefanst77 <46823784+stefanst77@users.noreply.github.com>
2024-10-30 14:06:05 +0000

    Reference Variables checked

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -7,26 +7,38 @@ public class MenuBuilder {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
+    String menuTitle = "My Dream Menu:";
 
     // Print the menuTitle variable to the console.
+    System.out.println(menuTitle);
 
     // Create a variable called menu of type ArrayList.
+    ArrayList<MenuItem> menu = new ArrayList<>();
 
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
+    MenuItem starter = new MenuItem("paine");
+    // System.out.println(starter.toString());
 
     // Add the starter variable to the ArrayList called menu.
+    menu.add(starter);
+    //System.out.println(menu);
 
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
+    MenuItem mainCourse = new MenuItem("steak");
 
     // Add the mainCourse variable to the ArrayList called menu.
+    menu.add(mainCourse);
 
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
+    MenuItem dessert = new MenuItem("carrot cake");
 
     // Add the dessert variable to the ArrayList called menu.
+    menu.add(dessert);
 
     // Print the menu variable to the console.
+    System.out.println(menu);
   }
 }
